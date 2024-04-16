505d246a94f35905c3b1f9e0b8d1bdcbe687bee6
sumanmanna134 <manna.suman134@gmail.com>
2024-04-16 16:09:21 +0530

    remove unsued imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SystemModule } from './system/system.module';
-import { SystemService } from './system/system.service';
 
 @Module({
   imports: [SystemModule, SystemModule],
