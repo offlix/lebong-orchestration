7f58dc05a5d5096457c428930f6675d012ac4ba3
sumanmanna134 <manna.suman134@gmail.com>
2024-04-16 16:31:52 +0530

    fix

--- src/system/system.controller.ts
+++ src/system/system.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { SystemService } from './system.service';
 
 @Controller('system')
